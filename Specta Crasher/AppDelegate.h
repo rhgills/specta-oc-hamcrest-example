f0ed20756cb384e4d83a4cc7d8b91e6d6ba85df7
Robert Gilliam <robert@robertgilliam.org>
2013-06-17 13:41:45 -0700

    Initial Commit

---
+++ Specta Crasher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Specta Crasher
+//
+//  Created by Robert Gilliam on 6/17/13.
+//  Copyright (c) 2013 Robert Gilliam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
